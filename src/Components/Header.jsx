62ef30c7dc5eef130fdb1be3b075b733b97971bb
Babatunde Yusuf Folorunsho <babatundeyusuffolorunsho@gmail.com>
2022-11-25 09:02:11 +0100

    updating

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from "react"
+import { Link } from "react-router-dom";
 // import logo from "./Components/images/Earn Reach.png"
 // import A from "../Components/images/a.jpg"
 
@@ -15,14 +16,14 @@ class Header extends Component{
               
                     </div>
                         <ol>
-                            <li>Home</li>
-                            <li>News</li>
-                            <li>MarketPlace</li>
-                            <li>About</li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to='/postads'>News</Link></li>
+                            <li><Link to='/side'>MarketPlace</Link></li>
+                            <li><Link to='/content'>About</Link></li>
                         </ol>
               </div>
 
-              <div>Sign In</div>
+              <div><Link to='/'>Sign In</Link></div>
             
             
             </header>
